fe74c9f1f543a1a13ed839671ae000d652e7822b
alfygitau <alfygitau@gmail.com>
2025-01-14 03:08:48 +0300

    changes

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -18,8 +18,6 @@ const ContactUs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
-    console.log("Form submitted:", formData);
   };
 
   return (
